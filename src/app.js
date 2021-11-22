a667cf024e75a56c60966224478ab6fbde0c3586
Chrissy Addy <chrissy.addy@gmail.com>
2021-11-22 10:41:38 -0600

    Refactored code

--- src/app.js
+++ src/app.js
@@ -2,16 +2,10 @@
 
 function formatDate(timestamp) {
   // Calculate the date
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let days = [
+  const date = new Date(timestamp);
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const days = [
     'Sunday',
     'Monday',
     'Tuesday',
@@ -20,18 +14,27 @@ function formatDate(timestamp) {
     'Friday',
     'Saturday',
   ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  let amPM;
+
+  if (hours > 12) {
+    hours -= 12;
+    amPM = 'pm';
+  } else {
+    amPM = 'am';
+  }
+
+  const day = days[date.getDay()];
+  return `${day} ${hours}:${minutes} ${amPM}`;
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
-  let cityElement = document.querySelector('#city');
-  let temperatureElement = document.querySelector('#temperature');
-  let descriptionElement = document.querySelector('#description');
-  let humidityElement = document.querySelector('#humidity');
-  let windElement = document.querySelector('#wind');
-  let dateElement = document.querySelector('#date');
+  const cityElement = document.getElementById('city');
+  const temperatureElement = document.getElementById('temperature');
+  const descriptionElement = document.getElementById('description');
+  const humidityElement = document.getElementById('humidity');
+  const windElement = document.getElementById('wind');
+  const dateElement = document.getElementById('date');
+  const iconElement = document.getElementById('icon');
 
   humidityElement.innerHTML = response.data.main.humidity;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -39,9 +42,11 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 }
-let apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
+const apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
 let city = 'Austin';
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let units = 'metric';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(displayTemperature);
