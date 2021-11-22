75447ffdcb69889110e6476673f291104ef2710b
Chrissy Addy <chrissy.addy@gmail.com>
2021-11-22 11:44:21 -0600

    Added search engine

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 function formatDate(timestamp) {
-  // Calculate the date
   const date = new Date(timestamp);
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
@@ -36,17 +35,31 @@ function displayTemperature(response) {
   const dateElement = document.getElementById('date');
   const iconElement = document.getElementById('icon');
 
-  humidityElement.textContent = response.data.main.humidity;
-  temperatureElement.textContent = Math.round(response.data.main.temp);
   cityElement.textContent = response.data.name;
+  temperatureElement.textContent = Math.round(response.data.main.temp);
   descriptionElement.textContent = response.data.weather[0].description;
+  humidityElement.textContent = response.data.main.humidity;
   windElement.textContent = Math.round(response.data.wind.speed);
   dateElement.textContent = formatDate(response.data.dt * 1000);
   iconElement.src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 }
-const apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
-let city = 'Austin';
-let units = 'metric';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  const apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
+  let units = 'metric';
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.getElementById('city-input');
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+search('Austin');
+
+const form = document.getElementById('search-form');
+form.addEventListener('submit', handleSubmit);
