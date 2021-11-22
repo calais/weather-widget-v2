b79121d3844b6623c7e3e729b850f228a460d53c
Chrissy Addy <chrissy.addy@gmail.com>
2021-11-22 10:07:09 -0600

    Update time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 'use strict';
 
+function formatDate(timestamp) {
+  // Calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let cityElement = document.querySelector('#city');
@@ -7,12 +31,14 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector('#description');
   let humidityElement = document.querySelector('#humidity');
   let windElement = document.querySelector('#wind');
+  let dateElement = document.querySelector('#date');
 
   humidityElement.innerHTML = response.data.main.humidity;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
 let city = 'Austin';
