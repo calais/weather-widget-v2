1cc733753876d0eff3b6197f7dc00bf42d25fc7f
Chrissy Addy <chrissy.addy@gmail.com>
2021-11-22 09:52:28 -0600

    Retrieving weather data from API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector('#city');
+  let temperatureElement = document.querySelector('#temperature');
+  let descriptionElement = document.querySelector('#description');
+  let humidityElement = document.querySelector('#humidity');
+  let windElement = document.querySelector('#wind');
+
+  humidityElement.innerHTML = response.data.main.humidity;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
+let city = 'Austin';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
