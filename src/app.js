71fcbcf132d6ed39f13d0eea0a90e628a189378b
Chrissy Addy <chrissy.addy@gmail.com>
2021-12-19 11:00:10 -0600

    Getting forecast data from API

--- src/app.js
+++ src/app.js
@@ -2,20 +2,22 @@
 
 let units = 'imperial';
 let city = 'Austin';
+const apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
+const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
 
 function formatDate(timestamp) {
   const date = new Date(timestamp);
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
+  
   let amPM;
 
   if (hours > 12) {
@@ -25,26 +27,35 @@ function formatDate(timestamp) {
     amPM = 'am';
   }
 
-  const day = days[date.getDay()];
+  const day = daysOfWeek[date.getDay()];
   return `${day} ${hours}:${minutes} ${amPM}`;
 }
 
-function displayForecast() {
-  let forecastElement = document.getElementById('forecast');
-
-  let days = ["Thursday", "Friday", "Saturday"];
+function displayForecast(response) {
+  const forecastElement = document.getElementById('forecast');
+  console.log(response.data)
+;
+  let forecastDays = [1, 2, 3, 4, 5];
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day){
+  forecastDays.forEach(function (day){
+    const max = Math.round(response.data.daily[day].temp.max);
+    const min = Math.round(response.data.daily[day].temp.min);
+
+    const timestamp = response.data.daily[day].dt;
+    const forecastDate = new Date(timestamp * 1000);
+    const dayOfWeek = daysOfWeek[forecastDate.getDay()];
+ 
+
     forecastHTML =
       forecastHTML +
       `
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
-                <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="42" />
+                <div class="weather-forecast-date">${dayOfWeek}</div>
+                <img src="http://openweathermap.org/img/wn/${response.data.daily[day].weather[0].icon}@2x.png" alt="" width="42" />
                 <div class="weather-forecast-temp">
-                  <span class="weather-forecast-temp-max">18° </span
-                  ><span class="weather-forecast-temp-min">12°</span>
+                  <span class="weather-forecast-temp-max">${max}° </span
+                  ><span class="weather-forecast-temp-min">${min}°</span>
                 </div>
               </div>`;
   })
@@ -63,6 +74,12 @@ function displayWeather(response) {
   const dateElement = document.getElementById('date');
   const iconElement = document.getElementById('icon');
 
+  const lat = response.data.coord.lat;
+  console.log(lat);
+  const lon = response.data.coord.lon;
+
+  const forecastAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+
   cityElement.textContent = response.data.name;
   temperatureElement.textContent = Math.round(response.data.main.temp);
   descriptionElement.textContent = response.data.weather[0].description;
@@ -70,13 +87,16 @@ function displayWeather(response) {
   windElement.textContent = Math.round(response.data.wind.speed);
   dateElement.textContent = formatDate(response.data.dt * 1000);
   iconElement.src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+
+  axios.get(forecastAPI).then(displayForecast);
 }
 
 function search(city) {
-  const apiKey = '204af6a06d59739ba0c43dfe8c56a8ca';
+  
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(displayWeather);
+  
 }
 
 function handleSubmit(event) {
@@ -118,4 +138,4 @@ const celciusLink = document.getElementById('celcius-link');
 celciusLink.addEventListener('click', changeToMetric);
 
 search(city);
-displayForecast();
+// displayForecast();
